c9fae7415576d30761ad9459148bb61754c1ff3d
Firoz Khan <>
2022-06-15 18:07:18 +0530

    Added a new collection view cell with capability to handle ColumnGroupType Update the naming of columnView

---
+++ SwiftTabularUI/TabularView/CutomeComponents/ColumnCell/ColumnCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ColumnCollectionViewCell.swift
+//  SwiftTabularUI
+//
+//  Created by Firoz Khan on 15/06/22.
+//
+
+import UIKit
+
+class ColumnCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SwiftTabularUI/TabularView/CutomeComponents/ColumnsView/ColumnsView.swift
@@ -0,0 +1,67 @@
+//
+//  ColumnsView.swift
+//  SwiftTabularUI
+//
+//  Created by Firoz Khan on 15/06/22.
+//
+
+import UIKit
+
+enum ColumnGroupType {
+    case single
+    case double
+    case triple
+    
+    var cellIdentifier:String {
+        switch self {
+        case .single:
+            return ""
+        case .double:
+            return ""
+        case .triple:
+            return ""
+        }
+    }
+}
+
+class ColumnsView: XIBView {
+    private let cellIdentifier = "ColumnCollectionViewCell"
+    @IBOutlet private(set) var collectionView:UICollectionView!
+    
+    override func innerContentView() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        bundle.loadNibNamed("ColumnsView", owner: self, options: nil)
+        return self.contentView
+    }
+    
+    override func didLoadView() {
+        setupCollectionView()
+    }
+}
+
+extension ColumnsView: UICollectionViewDataSource {
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let nib = UINib(nibName: cellIdentifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: cellIdentifier)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier:cellIdentifier , for: indexPath) as? ColumnCollectionViewCell
+        else { return UICollectionViewCell() }
+        return cell
+    }
+}
+
+extension ColumnsView: UICollectionViewDelegate {
+    
+}
