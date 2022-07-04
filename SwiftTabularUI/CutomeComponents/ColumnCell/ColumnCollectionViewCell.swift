4141fc79224bc3af47a53a20aae5c67c98eab98e
Firoz Khan <>
2022-07-04 15:19:05 +0530

    Done with adding basic plain items on collection cell

--- SwiftTabularUI/CutomeComponents/ColumnCell/ColumnCollectionViewCell.swift
+++ SwiftTabularUI/CutomeComponents/ColumnCell/ColumnCollectionViewCell.swift
@@ -27,6 +27,31 @@ class ColumnCollectionViewCell: UICollectionViewCell {
     
     private func loadViews(for columnData: ColumnInfoData) {
         removeAll()
+        addHeader(columnData.header)
+        addItems(columnData.items)
+        addFooter(columnData.footer)
+    }
+    
+    private func addHeader(_ header:ColumnHeader) {
+        let headerView = TabularLabelView(frame: .zero)
+        headerView.label.text = header.title
+        containerStackView.addArrangedSubview(headerView)
+    }
+    
+    private func addItems(_ items:[RowItem]) {
+        items.forEach { rowItem in
+            let itemView = TabularLabelView(frame: .zero)
+            itemView.label.text = rowItem.value
+            containerStackView.addArrangedSubview(itemView)
+        }
+    }
+    
+    private func addFooter(_ footer:ColumnFooter?) {
+        if let value = footer?.value {
+            let footerView = TabularLabelView(frame: .zero)
+            footerView.label.text = value
+            containerStackView.addArrangedSubview(footerView)
+        }
     }
     
     private func removeAll() {
